1c8bdc84c2af2164c28b16e0c073cd4142ca30cc
Prince Gbezeh <gbezehp@gmail.com>
2024-04-23 18:31:10 +0000

    Initial commit

---
+++ backend/notes/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from .views import NoteListCreateView, NoteDeleteView
+
+
+urlpatterns = [
+    path("notes/", NoteListCreateView.as_view(), name="note-list"),
+    path("notes/delete/<int:pk>/", NoteDeleteView.as_view(), name="delete-note"),
+]
---
+++ backend/notes/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+from .serializers import UserSerializer, NoteSerializer
+from .models import Note
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+
+# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
+
+
+class NoteListCreateView(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+
+class NoteDeleteView(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
